694f66f7ad65965c3c718e2d35e1980d2f222245
tanawaii <ryotatnaka@gmail.com>
2021-11-23 06:29:02 +0900

    feat: 基本機能の追加 メッセージ送受信機能 時刻表示機能
    close #11

--- src/component/ChatViewer/index.tsx
+++ src/component/ChatViewer/index.tsx
@@ -1,4 +1,25 @@
-function ChatViewer() {
-  return <div></div>;
+import dayjs from "dayjs";
+
+type ChatViewerProps = {
+  chatMessage: {
+    user: string;
+    text: string;
+    meta: { type: "system" | "sendMessage" | "receiveMessage" };
+    date: string;
+  }[];
+};
+
+function ChatViewer(props: ChatViewerProps) {
+  return (
+    <div>
+      {props.chatMessage.map((line) => {
+        return (
+          <p key={line.text + line.date}>
+            {line.text}:{dayjs(line.date).format("HH:mm:ss")}
+          </p>
+        );
+      })}
+    </div>
+  );
 }
 export default ChatViewer;
